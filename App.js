a302edd43078eb389f20fafa49040df9b773ebe9
BenDev181 <benjiro181@gmail.com>
2024-10-22 12:10:43 +0100

    Day 2 key tasks complete

--- App.js
+++ App.js
@@ -1,10 +1,13 @@
-import React from 'react';
+import React, {useState} from 'react';
 // Import data and WeatherCard here
 const cities = require("./data.js")
 const WeatherCard = require("./components/WeatherCard.js")
+const Location = require("./components/Location.js")
+
 
 
 function App() {
+    const [location, setLocation] = useState("New York City")
     return (
         <>
             <h1 className = "title">REACTIVE WEATHER</h1>
@@ -12,10 +15,8 @@ function App() {
             <div className = "app">
                 {cities.map(city => (
                     <WeatherCard data={city} />
-                ))}
-                
-
-
+                ))};
+                <Location data={cities} location={location} setLocation={setLocation}/>
             </div>
             
         </>
---
+++ components/Location.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Location(props) {
+    return (
+        <>
+        
+        </>
+    )
+}
+
+
+
+
+module.exports = Location
