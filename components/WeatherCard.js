5c91309ab1e97b15fbd3db04d5a2d88104d2bdec
BenDev181 <benjiro181@gmail.com>
2024-10-22 11:25:31 +0100

    Conditional weather pictures in day 2

--- components/WeatherCard.js
+++ components/WeatherCard.js
@@ -1,12 +1,23 @@
 import React from "react";
-import sunny from "../assets/Sunny.svg"
+import Sunny from "../assets/Sunny.svg"
+import Cloudy from "../assets/Cloudy.svg"
+import PartlyCloudy from "../assets/PartlyCloudy.svg"
+import Rainy from "../assets/Rainy.svg"
+
+const forecastImages = {
+  Sunny: Sunny,
+  Cloudy: Cloudy,
+  Rainy: Rainy,
+  'Partly cloudy': PartlyCloudy,
+  Snowy: Rainy
+}
 
 
 function WeatherCard(props) {
   return (
     <div className = "card">
         <div className = "img-container">
-            <img className="card-img-top" src = {sunny} alt="Card image cap" id = "icon"/>
+            <img className="card-img-top" src = {forecastImages[props.data.forecast]} alt="Card image cap" id = "icon"/>
         </div>
         <div class="card-body">
             <h3 className="card-title">{props.data.city}</h3>
